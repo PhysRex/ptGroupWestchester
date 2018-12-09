78f007928989878a0f9244b23fbb9fc101010e85
PhysRex <ThePhysRex@gmail.com>
2018-12-09 11:05:49 -0500

    created general purpose download link component

--- src/components/templates/DownloadLink.js
+++ src/components/templates/DownloadLink.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
-
+/**
+ * Creates a link which will automatically download content
+ * @param {object} options The options to create a link to downloadable content
+ * @param {object} text The link display text
+ * @param {object} contentToDownload The file location to the downloadable content, for a directory within the `src`
+ * @param {object} className Any classNames that the link should have
+ * @returns {undefined}
+ */
 const DownloadLink = ({ text, contentToDownload, className }) => (
-  <a className={className} href={contentToDownload} target='_blank' download>{text}</a>
+  <a
+    className={className}
+    href={contentToDownload}
+    target='_blank'
+    download
+  >
+    {text}
+  </a>
 );
 
 
