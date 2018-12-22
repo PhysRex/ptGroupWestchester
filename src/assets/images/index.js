ebc5e1109868388d8af3446e87b26fa3431d7807
PhysRex <ThePhysRex@gmail.com>
2018-12-22 09:30:13 -0500

    adding additional test images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,10 +1,16 @@
 import practiceOutside from './PracticeOutside-v3.png';
 import practiceAerialView from './PracticeAerialView-v2.png';
 import hengAndFamily from './HengAndFamily-v2.jpg';
+import mauricioPortrait from './mauricioPortrait.jpg';
+import profile1 from './profile-1.jpg';
+import profile2 from './profile-2.jpg';
 
 
 export {
   practiceOutside,
   practiceAerialView,
   hengAndFamily,
+  mauricioPortrait,
+  profile1,
+  profile2,
 }
