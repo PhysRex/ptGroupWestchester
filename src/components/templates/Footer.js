36d70521dc30dc1928058ef5a09dbdeda1123025
PhysRex <ThePhysRex@gmail.com>
2018-12-23 00:15:51 -0500

    created base footer element

---
+++ src/components/templates/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  Container,
+  Grid,
+  Header,
+} from 'semantic-ui-react';
+
+
+/**
+ * A generalized footer component
+ * @returns {JSX} The rendered ReactJS component
+ */
+export const Footer = () => (
+  <Grid className="bg-primary">
+    <Container className="margin-20">
+      <Grid stackable>
+        <Grid.Row className="" columns={1}>
+          <Grid.Column mobile={16} tablet={16} computer={16}>
+            <Header as='h2' textAlign='center' className="">
+              Footer component
+              <Header.Subheader>
+                Should include hours, logo info, etc...
+              </Header.Subheader>
+            </Header>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+  </Grid>
+);
+
+
+export default Footer;
