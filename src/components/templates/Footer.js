a9ff757354884d5ad4e6537a4b71dc225ef7330e
PhysRex <ThePhysRex@gmail.com>
2019-03-06 20:30:48 -0500

    updated logo for footer, made footer include logo

--- src/components/templates/Footer.js
+++ src/components/templates/Footer.js
@@ -5,23 +5,54 @@ import {
   Header,
 } from 'semantic-ui-react';
 
+import { Logo } from './';
+import {
+  homePage,
+  menuPages,
+} from '../../components/navigation/config';
+import  { shared } from '../../copy';
+
 /**
  * A generalized footer component
  * @returns {JSX} The rendered ReactJS component
  */
-
 export const Footer = () => (
   <Grid className="bg-primary" centered>
     <Container className="margin-20">
       <Grid stackable>
         <Grid.Row className="" columns="equal">
           <Grid.Column mobile={16} tablet={16} computer={16}>
+          
+            <Logo
+              title={shared.site.title}
+              subtitle={shared.site.subtitle}
+              homePage={homePage}
+              // handleClick={props.setNavHome}
+              isHorizontal
+            />
+
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row className="" columns="equal">
+         <Grid.Column mobile={16} tablet={8} computer={8}>
+
             <Header as='h2' textAlign='center' className="">
               Footer component
               <Header.Subheader>
                 Should include hours, logo info, etc...
               </Header.Subheader>
             </Header>
+
+          </Grid.Column>
+         <Grid.Column mobile={16} tablet={8} computer={8}>
+
+            <Header as='h2' textAlign='center' className="">
+              Footer component
+              <Header.Subheader>
+                Should include hours, logo info, etc...
+              </Header.Subheader>
+            </Header>
+
           </Grid.Column>
         </Grid.Row>
       </Grid>
--- src/components/templates/Logo.js
+++ src/components/templates/Logo.js
@@ -5,15 +5,15 @@ import {
 } from 'semantic-ui-react';
 
 
-const Logo = ({ title, subtitle, homePage, handleClick }) => (
+const Logo = ({ title, subtitle, homePage, handleClick, isHorizontal = false }) => (
   <Link to={`/${homePage}`} onClick={handleClick} className="no-link f-text-white">
-    <div className="flex align-items-center height-fill padding-left-10">
+    <div className={`flex align-items-center padding-left-10 ${isHorizontal ? 'flex-direction-column' : 'height-fill'}`}>
       <Icon
         name='home'
         size='big'
         className="text-shadow-default"
       />
-      <div className="flex align-items-center flex-direction-column">
+      <div className={`flex align-items-center flex-direction-column ${isHorizontal ? 'margin-top-10' : ''}`}>
         <span className="font-lgr font-family-merriweather text-shadow-default">{title}</span>
         <span className="font-lgr font-family-sofia text-shadow-default">{subtitle}</span>
       </div>
