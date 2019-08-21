c7b6229c78ea3a83dbb50270573126db404af495
PhysRex <ThePhysRex@gmail.com>
2019-08-20 21:46:57 -0400

    change referral banner style, fix mobile view

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -24,7 +24,8 @@ const Home = () => (
       subheader='We participate with many insurance plans from a variety of insurance companies. We always check your physical therapy benefits, prior to your visit, and we would be happy to discuss them with you throughout your care. If you have any questions about whether we take your insurance, please give us a call or send us an email, and we will get back to you with all the necessary information as soon as possible.'
     />
     <ReferralBanner
-      className="banner-image-person-with-laptop flex align-items-center justify-content-center text-shadow-medium"
+      className="font-lgr text-align-center margin-top-bottom-70"
+      content='We are proud to be a participating member of the Hospital for Special Surgery - Rehabilitation Network.  We maintain a strong level of communication with all of our referring doctors by directly forwarding our reports and by keeping them updated on any progress or limitations patients may have.  We are always willing to have a conversation with your doctors to improve your care and experience at our office'
     />
   </Container>
 );
--- src/components/pages/Home/ReferralBanner.js
+++ src/components/pages/Home/ReferralBanner.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {
+  Container,
   Grid,
   Image,
  } from 'semantic-ui-react';
+ import { TextContent } from '../../templates/'
 
  import { hssLogo } from '../../../assets/images';
 
+
+ const defaultFont = 'alegreya';
 /**
  * Creates a banner with a logo and quote
  */
-const ReferralBanner = ({ className }) => (
-  // <Container>
+const ReferralBanner = ({ className, content, fontName }) => (
+  <Container>
     <Grid
       stackable
       padded
@@ -18,27 +22,29 @@ const ReferralBanner = ({ className }) => (
     >
       <Grid.Row
         centered
-        // columns={3}
         className="margin-top-bottom-70"
       >
-      <Grid.Column mobile={16} computer={6}>
-        <Image
-          src={hssLogo}
-          as='a'
-          size='medium'
-          href='https://www.hss.edu/find-a-therapy-provider_pt-group-of-westchester.asp'
-          target='_blank'
-          alt='Participating Member - Physical Therapy Group of Westchester'
-          title='Participating Member - Physical Therapy Group of Westchester'
-        />
-      </Grid.Column>
-      <Grid.Column mobile={16} computer={10} className="flex align-items-center justify-content-center">
-        <p className="">We are proud to be a participating member of the Hospital for Special Surgery - Rehabilitation Network.  We maintain a strong level of communication with all of our referring doctors by directly forwarding our reports and by keeping them updated on any progress or limitations patients may have.  We are always willing to have a conversation with your doctors to improve your care and experience at our office
-        </p>
-      </Grid.Column>
+        <Grid.Column mobile={16} computer={6} verticalAlign='middle'>
+          <Image
+            src={hssLogo}
+            className="margin-top-bottom-70 referral-banner--image"
+            as='a'
+            size='medium'
+            href='https://www.hss.edu/find-a-therapy-provider_pt-group-of-westchester.asp'
+            target='_blank'
+            alt='Participating Member - Physical Therapy Group of Westchester'
+            title='Participating Member - Physical Therapy Group of Westchester'
+          />
+        </Grid.Column>
+        <Grid.Column mobile={16} computer={10} className="flex align-items-center justify-content-center">
+          <TextContent
+            className={`font-family-${fontName || defaultFont} ${className}`}
+            content={content}
+          />
+        </Grid.Column>
       </Grid.Row>
     </Grid>
-  // </Container>
+  </Container>
 );
 
 
