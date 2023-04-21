fb0293c34231ebd22e59203f26fc0e96e21c6fc3
Mauricio Campuzano <ThePhysRex@gmail.com>
2023-04-20 23:22:58 -0400

    add new logo, change primary color to black

--- src/components/templates/Footer.js
+++ src/components/templates/Footer.js
@@ -9,19 +9,19 @@ import { Logo } from './';
 import {
   homePage,
 } from '../../components/navigation/config';
-import  { shared } from '../../copy';
+import { shared } from '../../copy';
 
 /**
  * A generalized footer component
  * @returns {JSX} The rendered ReactJS component
  */
 export const Footer = () => (
-  <Grid className="bg-primaryLight" centered>
+  <Grid className="bg-primary" centered>
     <Container className="margin-20">
       <Grid stackable>
         <Grid.Row className="" columns="equal">
           <Grid.Column mobile={16} tablet={16} computer={16}>
-          
+
             <Logo
               title={shared.site.title}
               subtitle={shared.site.subtitle}
@@ -32,35 +32,35 @@ export const Footer = () => (
 
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row className="margin-bottom-20" columns="equal">
-         <Grid.Column mobile={16} tablet={8} computer={8}>
+        <Grid.Row className="margin-bottom-20 text-white" columns="equal">
+          <Grid.Column mobile={16} tablet={8} computer={8}>
 
-            <Header as='h2' textAlign='center' className="">
+            <Header as='h2' textAlign='center' className="f-text-white">
               Visit Us
-              <Header.Subheader>
+              <Header.Subheader className="f-text-white">
                 <div>{shared.business.hoursShort}</div>
               </Header.Subheader>
             </Header>
-            <div> 
+            <div>
               <div>{shared.business.address.street}</div>
               <div>Suite {shared.business.address.suite}</div>
               <div>{shared.business.address.city}, {shared.business.address.state} {shared.business.address.zipCode}</div>
             </div>
 
           </Grid.Column>
-         <Grid.Column mobile={16} tablet={8} computer={8}>
+          <Grid.Column mobile={16} tablet={8} computer={8}>
 
-            <Header as='h2' textAlign='center' className="">
+            <Header as='h2' textAlign='center' className="f-text-white">
               Contact Us
-              <Header.Subheader>
+              <Header.Subheader className="f-text-white">
                 Please give us a call anytime
               </Header.Subheader>
             </Header>
             {/* TODO: fix how this looks */}
-            <div> 
-              <div>Phone: <a className="link-white" href={`tel:+1${shared.business.telephone}`}>{shared.business.telephone}</a></div>
+            <div>
+              <div>Phone: <a className="link-white text-underline font-weight-600" href={`tel:+1${shared.business.telephone}`}>{shared.business.telephone}</a></div>
               <div>Fax: {shared.business.fax}</div>
-              <div>Email: <a className="link-white" href={`mailto:${shared.business.email}`}>{shared.business.email}</a></div>
+              <div>Email: <a className="link-white text-underline font-weight-600" href={`mailto:${shared.business.email}`}>{shared.business.email}</a></div>
             </div>
 
           </Grid.Column>
--- src/components/templates/Logo.js
+++ src/components/templates/Logo.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-  Icon,
-} from 'semantic-ui-react';
+import { Icon, Image } from 'semantic-ui-react';
+import newLogo from '../../assets/images/logo/logo-idea-narrow.png';
+
+const isNew = true;
 
 
 const Logo = ({ title, subtitle, homePage, handleClick, isHorizontal = false }) => (
   <Link to={`/${homePage}`} onClick={handleClick} className="no-link f-text-white">
-    <div className={`flex align-items-center padding-left-10 ${isHorizontal ? 'flex-direction-column' : 'height-fill'}`}>
-      <Icon
-        name='home'
-        size='big'
-        className="text-shadow-default"
-      />
-      <div className={`flex align-items-center flex-direction-column ${isHorizontal ? 'margin-top-10' : ''}`}>
-        <span className="font-lgr font-family-merriweather text-shadow-default">{title}</span>
-        <span className="font-lgr font-family-sofia text-shadow-default">{subtitle}</span>
+    {isNew
+      ? <div className={`flex align-items-center padding-left-10 ${isHorizontal ? 'flex-direction-column' : 'height-fill'}`}>
+        <Image src={newLogo} size={'small'} />
       </div>
-    </div>
+      : <div className={`flex align-items-center padding-left-10 ${isHorizontal ? 'flex-direction-column' : 'height-fill'}`}>
+        <Icon
+          name='home'
+          size='big'
+          className="text-shadow-default"
+        />
+        <div className={`flex align-items-center flex-direction-column ${isHorizontal ? 'margin-top-10' : ''}`}>
+          <span className="font-lgr font-family-merriweather text-shadow-default">{title}</span>
+          <span className="font-lgr font-family-sofia text-shadow-default">{subtitle}</span>
+        </div>
+      </div>
+    }
   </Link>
 );
 
